81a9f3015dcad7cad3c72f795a77a724ee2edc30
Daniil <dan.bardyuk@yandex.ru>
2022-11-11 20:01:54 +0300

    add pagination

--- src/MainWindow.jsx
+++ src/MainWindow.jsx
@@ -1,38 +1,84 @@
 import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link, useLocation, useSearchParams } from "react-router-dom";
 import CharacterCard from "./component/CharactersCard";
 import FilterBar from "./component/FilterBar";
 
 export default function MainWindow() {
   const [characters, setCharacters] = useState([]);
+  const [pages, setPages] = useState({ prev: "", next: "" });
+  const location = useLocation();
+
   async function fetchCharaters() {
     try {
-      const response = await fetch("https://rickandmortyapi.com/api/character");
-      const { results } = await response.json();
-      console.log(results);
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character${location.search}`
+      );
+      const { info, results } = await response.json();
+      console.log(getPageFromUrl("next", info.next));
+      console.log("prev", getPageFromUrl(info.prev));
+      console.log(info);
       setCharacters(results);
+      setPages({
+        prev: getPageFromUrl(info.prev),
+        next: getPageFromUrl(info.next),
+      });
     } catch (error) {
       throw new Error(error);
     }
   }
 
+  const getPageFromUrl = (url) => {
+    if (url) {
+      return url.substring(url.indexOf("?"));
+    }
+    return "";
+  };
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const query = searchParams.get("species") || "";
+
+  const speciesFilterCharacter = (value) => {
+    if (value) {
+      setSearchParams({ species: value });
+    } else {
+      searchParams.delete("species");
+      setSearchParams(searchParams);
+    }
+  };
+
   useEffect(() => {
     fetchCharaters();
-  }, []);
+  }, [location]);
+
   return (
     <>
-      <FilterBar></FilterBar>
-      <div className="max-w-7xl mx-auto grid grid-cols-4 gap-4 py-6 px-12">
-        {characters.map((el) => (
-          <NavLink key={el.id} to={`/character/${el.id}`}>
-            <CharacterCard
-              src={el.image}
-              name={el.name}
-              status={el.status}
-              species={el.species}
-            />
-          </NavLink>
-        ))}
+      <FilterBar
+        query={query}
+        speciesFilterCharacter={speciesFilterCharacter}
+      ></FilterBar>
+      <div className="box-content max-w-7xl mx-auto py-6 px-12">
+        <div className=" grid grid-cols-4 gap-4 ">
+          {characters.map((el) => (
+            <NavLink key={el.id} to={`/character/${el.id}`}>
+              <CharacterCard
+                src={el.image}
+                name={el.name}
+                status={el.status}
+                species={el.species}
+              />
+            </NavLink>
+          ))}
+        </div>
+        <div className="flex justify-end mt-4">
+          <p className="mr-12">Page 1 of 42</p>
+          <nav>
+            <Link to={`/characters${pages.prev}`} className="mr-2">
+              &lt;&lt;Prev
+            </Link>
+            <Link to={`/characters${pages.next}`}>Next&gt;&gt;</Link>
+          </nav>
+        </div>
       </div>
     </>
   );
--- src/component/FilterBar.jsx
+++ src/component/FilterBar.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { useSearchParams, NavLink } from "react-router-dom";
 import SearchInput from "./SearchInput";
 
-export default function FilterBar() {
+export default function FilterBar({ speciesFilterCharacter, query }) {
   const filter = [
-    { id: "species-All", value: "All" },
-    { id: "species-Human", value: "Human" },
-    { id: "species-Animal", value: "Animal" },
-    { id: "species-Alien", value: "Alien" },
+    { id: "species-All", value: "All", species: "" },
+    { id: "species-Human", value: "Human", species: "Human" },
+    { id: "species-Animal", value: "Animal", species: "Animal" },
+    { id: "species-Alien", value: "Alien", species: "Alien" },
   ];
 
-  const [searchParams] = useSearchParams();
   return (
     <section className=" bg-[#f2f2f2] py-4">
       <div className="max-w-7xl mx-auto flex px-12 items-center">
@@ -18,7 +16,10 @@ export default function FilterBar() {
           {filter.map((el) => (
             <label
               key={el.id}
-              className=" py-1 px-2 border-black cursor-pointer hover:bg-neutral-500"
+              onClick={() => speciesFilterCharacter(el.species)}
+              className={`${
+                el.species === query ? "activeStyle" : "hover:bg-neutral-500"
+              } py-1 px-2 cursor-pointer `}
             >
               <span>{el.value}</span>
             </label>
