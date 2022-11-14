adee4f6a827e125650c1329fdb643fc80a21ebb5
Daniil <dan.bardyuk@yandex.ru>
2022-11-14 19:59:58 +0300

    fix query

--- src/MainWindow.jsx
+++ src/MainWindow.jsx
@@ -2,41 +2,29 @@ import React, { useEffect, useState } from "react";
 import { NavLink, Link, useLocation, useSearchParams } from "react-router-dom";
 import CharacterCard from "./component/CharactersCard";
 import FilterBar from "./component/FilterBar";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToFavorite,
+  removeFromFavorite,
+  getAllCharacters,
+} from "./redux/actions/actionCreatore";
 
 export default function MainWindow() {
-  const [characters, setCharacters] = useState([]);
-  const [pages, setPages] = useState({ prev: "", next: "" });
+  const [searchParams, setSearchParams] = useSearchParams();
   const location = useLocation();
+  const dispatch = useDispatch();
+  const characters = useSelector((store) => store?.characters?.characters);
 
   async function fetchCharaters() {
     try {
-      const response = await fetch(
-        `https://rickandmortyapi.com/api/character${location.search}`
-      );
-      const { info, results } = await response.json();
-      console.log(getPageFromUrl("next", info.next));
-      console.log("prev", getPageFromUrl(info.prev));
-      console.log(info);
-      setCharacters(results);
-      setPages({
-        prev: getPageFromUrl(info.prev),
-        next: getPageFromUrl(info.next),
-      });
+      dispatch(getAllCharacters());
     } catch (error) {
       throw new Error(error);
     }
   }
-
-  const getPageFromUrl = (url) => {
-    if (url) {
-      return url.substring(url.indexOf("?"));
-    }
-    return "";
-  };
-
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const query = searchParams.get("species") || "";
+  const querySpecies = searchParams.get("species") || "";
+  const queryPage = +searchParams.get("page") || 1;
+  const queryString = new URLSearchParams(searchParams);
 
   const speciesFilterCharacter = (value) => {
     if (value) {
@@ -47,16 +35,38 @@ export default function MainWindow() {
     }
   };
 
+  const changePageNum = (num) => {
+    queryString.set("page", queryPage + num);
+    return queryString.toString();
+  };
+
   useEffect(() => {
+    window.scrollTo(0, 0);
     fetchCharaters();
   }, [location]);
+
+  const handleAdd = () => {
+    dispatch(addToFavorite());
+  };
+
+  const handleRemove = () => {
+    dispatch(removeFromFavorite());
+  };
 
   return (
     <>
       <FilterBar
-        query={query}
+        query={querySpecies}
         speciesFilterCharacter={speciesFilterCharacter}
-      ></FilterBar>
+      />
+      <div>
+        <button className="h-4 w-4 bg-indigo-500 mr-4" onClick={handleRemove}>
+          -
+        </button>
+        <button className="h-4 w-4 bg-indigo-500" onClick={handleAdd}>
+          +
+        </button>
+      </div>
       <div className="box-content max-w-7xl mx-auto py-6 px-12">
         <div className=" grid grid-cols-4 gap-4 ">
           {characters.map((el) => (
@@ -71,12 +81,12 @@ export default function MainWindow() {
           ))}
         </div>
         <div className="flex justify-end mt-4">
-          <p className="mr-12">Page 1 of 42</p>
+          <p className="mr-12">Page {queryPage} of 42</p>
           <nav>
-            <Link to={`/characters${pages.prev}`} className="mr-2">
+            <Link to={`/characters?${changePageNum(-1)}`} className="mr-2">
               &lt;&lt;Prev
             </Link>
-            <Link to={`/characters${pages.next}`}>Next&gt;&gt;</Link>
+            <Link to={`/characters?${changePageNum(+1)}`}>Next&gt;&gt;</Link>
           </nav>
         </div>
       </div>
